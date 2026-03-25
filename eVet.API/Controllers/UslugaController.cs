c1f5e7f9db2928762f7176aa8330e439c6de9041
Lamija Behram <behramlamija@gmail.com>
2026-03-26 00:11:10 +0100

    MainStructure: Developed main structure for eVet project

---
+++ eVet.API/Controllers/UslugaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eVet.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UslugaController : ControllerBase
+    {
+       
+    }
+}
